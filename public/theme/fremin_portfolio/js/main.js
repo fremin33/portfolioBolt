dd4c87d165de61c3d174d0e60a9ac5e9ef1ce386
fremin33 <florian.dev33@gmail.com>
2018-07-05 10:47:07 +0200

    update img

--- public/theme/fremin_portfolio/js/main.js
+++ public/theme/fremin_portfolio/js/main.js
@@ -29,4 +29,9 @@ $(document).ready(function(){
         $grid.isotope({ filter: filterValue });
     });
 
+    var url_string = "http://www.example.com/t.html?a=1&b=3&c=m2-m3-m4-m5"; //window.location.href
+var url = new URL(url_string);
+var c = url.searchParams.get("c");
+console.log(c);
+
 });
